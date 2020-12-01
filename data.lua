ad67caa237bec38d44f44d1a7ae0f05c1eecbcfa
adam wong <adamwong246@gmail.com>
2020-12-01 15:01:31 -0800

    init

---
+++ data.lua
@@ -0,0 +1,6 @@
+data.raw["character"]["character"].build_distance = 1
+data.raw["character"]["character"].drop_item_distance = 1
+data.raw["character"]["character"].item_pickup_distance = 1
+data.raw["character"]["character"].loot_pickup_distance = 1
+data.raw["character"]["character"].reach_distance = 1
+data.raw["character"]["character"].reach_resource_distance = 1
